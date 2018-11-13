a694cbcf94bdf1807f7af0b52ece482603472471
Wojtek Dudek <wdudek82@gmail.com>
2018-11-13 22:47:48 +0100

    Adding Owner List and updated controller. Closes #16.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -28,23 +28,23 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(1L);
+        owner2.setId(2L);
         owner2.setFirstName("Fiona");
-        owner2.setLastName("Glenannne");
+        owner2.setLastName("Glenanne");
 
         ownerService.save(owner2);
 
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
+        vet1.setId(3L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(1L);
+        vet2.setId(4L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
